19ad25350b02503918d41b8c5950e0289145fa25
Lella, Venugopala <Venugopala.Lella@ellucian.com>
2018-10-05 14:21:23 +0530

    added list features for s3 buckets

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,3 +1,26 @@
+import boto3
+import click
+
+session = boto3.Session ( profile_name='default' )
+s3 = session.resource ( 's3' ) 
+
+
+@click.group()
+def cli():
+   "Webotron deploys websites to AWS"
+
+@cli.command( 'list-buckets' ) 
+def list_buckets():
+   "List all S3 Buckets"
+   for b in s3.buckets.all():
+      print ( b )
+
+@cli.command ( 'list-bucket-objects' )
+@click.argument ( 'bucket' ) 
+def list_bucket_objects ( bucket ):
+   "List object in an S3 Bucket"
+   for obj in s3.Bucket(bucket).objects.all():
+      print ( obj ) 
 
 if __name__ == '__main__':
-   print ('Hello, World!')
+   cli()
--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Automation of AWS using Python-Three
 ## 01-webotron
 
 webotron is a script that will sync a local directory to an S3 bucket, optionally configure Route53 and cloudfront as well.
+
+### Features
+
+- List buckets
+- List contents of a bucket
